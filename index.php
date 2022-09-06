2335979804784f61fe095008cae2daf6e5988870
Alba Castano <acastanome@gmail.com>
2022-09-06 20:59:16 +0300

    index.php require_once config/setup.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'config/setup.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camagru</title>
+</head>
+<body>
+    <h1>WHAT</h1>
+    <?php
+    echo 'WHASSSUUUP';
+    ?>
+<h3>Welcome</h3>
+</body>
+<footer>
+<em>&copy; acastano | Hive 2022</em>
+</footer>
+</html>
