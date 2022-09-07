dbd222f0234651836fa6d3a8002ee2bc66b887c9
Alba Castano <acastanome@gmail.com>
2022-09-07 13:26:31 +0300

    backend/db_tables.php createTableImages() added column for name, path, creationTime, likes and comments

--- backend/db_tables.php
+++ backend/db_tables.php
@@ -1,23 +1,25 @@
 <?php
 function createTableUser($conn) {
-    $sql = "CREATE TABLE Users (
-        userId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        userName VARCHAR(30) NOT NULL,
-        email VARCHAR(50) NOT NULL,
-        confirmedAccount BOOLEAN DEFAULT 'false',
-        psswd VARCHAR(128) NOT NULL
-        notifications BOOLEAN DEFAULT 'true',
+    $sql = "CREATE TABLE 'Users' (
+        'userId' INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        'userName' VARCHAR(30) NOT NULL,
+        'email' VARCHAR(50) NOT NULL,
+        'confirmedAccount' BOOLEAN DEFAULT 'false',
+        'psswd' VARCHAR(128) NOT NULL
+        'notifications' BOOLEAN DEFAULT 'true',
         )";
     $conn->exec($sql);
 }
 
 function createTableImages($conn) {
-    $sql = "CREATE TABLE Images (
-        imageId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        userId INT(6) UNSIGNED NOT NULL,
-        creationTime
-        likes
-        comments
+    $sql = "CREATE TABLE 'Images' (
+        'imageId' INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        'userId' INT UNSIGNED NOT NULL,
+        'name' VARCHAR(30) NOT NULL,
+        'path' VARCHAR(250) NOT NULL DEFAULT '',
+        'creationTime' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        'likes' INT UNSIGNED NOT NULL;
+        'comments' INT UNSIGNED NOT NULL;
         )";
     $conn->exec($sql);
 }
@@ -31,7 +33,9 @@ function createTableStickers($conn) {
 }
 
 function addUserToTable() {
+//    $sql = "";
 }
+
 function deleteUserFromTable() {
 }
 ?>
