dfb2c5fade64e1d76f1da1a1187825ef3056fd8f
Alba Castano <acastanome@gmail.com>
2022-09-07 14:28:34 +0300

    fixed syntax of createTable..() functions. Left 2 ways to execute creation of tables with mysql database (conn->prepare execute and conn->exec)

--- backend/db_tables.php
+++ backend/db_tables.php
@@ -1,36 +1,33 @@
 <?php
-function createTableUser($conn) {
-    $sql = "CREATE TABLE 'Users' (
-        'userId' INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        'userName' VARCHAR(30) NOT NULL,
-        'email' VARCHAR(50) NOT NULL,
-        'confirmedAccount' BOOLEAN DEFAULT 'false',
-        'psswd' VARCHAR(128) NOT NULL,
-        'notifications' BOOLEAN DEFAULT 'true'
-        )";
-    $conn->exec($sql);
+function createTableUsers($conn) {
+    $sql = $conn->prepare("CREATE TABLE IF NOT EXISTS Users (
+        userId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        userName VARCHAR(30) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        confirmedAccount BOOLEAN DEFAULT false,
+        psswd VARCHAR(128) NOT NULL,
+        notifications BOOLEAN DEFAULT true)");
+    $sql->execute();
 }
 
 function createTableImages($conn) {
-    $sql = "CREATE TABLE 'Images' (
-        'imageId' INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        'userId' INT UNSIGNED NOT NULL,
-        'name' VARCHAR(30) NOT NULL,
-        'path' VARCHAR(250) NOT NULL DEFAULT '',
-        'creationTime' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        'likes' INT UNSIGNED NOT NULL,
-        'comments' INT UNSIGNED NOT NULL
-        )";
+    $sql = "CREATE TABLE IF NOT EXISTS Images (
+        imageId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        userId INT UNSIGNED NOT NULL,
+        name VARCHAR(30) NOT NULL,
+        path VARCHAR(250) NOT NULL DEFAULT '',
+        creationTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        likes INT UNSIGNED NOT NULL,
+        comments INT UNSIGNED NOT NULL)";
     $conn->exec($sql);
 }
 
 function createTableStickers($conn) {
-    $sql = "CREATE TABLE Stickers (
-        'stickerId' INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        'selected' BOOLEAN DEFAULT 'false',
-        'name' VARCHAR(30) NOT NULL,
-        'path' VARCHAR(250) NOT NULL DEFAULT ''
-        )";
+    $sql = "CREATE TABLE IF NOT EXISTS Stickers (
+        stickerId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        selected BOOLEAN DEFAULT false,
+        name VARCHAR(30) NOT NULL,
+        path VARCHAR(250) NOT NULL DEFAULT '')";
     $conn->exec($sql);
 }
 
