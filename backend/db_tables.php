a7b0733de05b5cc40aa92c9633ef889134b1b59c
Alba Castano <acastanome@gmail.com>
2022-09-13 21:26:38 +0300

    backend/db_tables.php selectOneQualifier() to look for a specific value in a specific column of a specific table. Functions to check if username or email exist before adding user, and function to add user to db working

--- backend/db_tables.php
+++ backend/db_tables.php
@@ -12,13 +12,12 @@ function createTableUsers($conn) {
 
 function createTableImages($conn) {
     $sql = "CREATE TABLE IF NOT EXISTS Images (
-        imageId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        imgId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         userId INT UNSIGNED NOT NULL,
-        name VARCHAR(30) NOT NULL,
-        path VARCHAR(250) NOT NULL DEFAULT '',
+        imgName VARCHAR(30) NOT NULL,
         creationTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        likes INT UNSIGNED NOT NULL,
-        comments INT UNSIGNED NOT NULL)";
+        likes INT UNSIGNED DEFAULT '0',
+        comments INT UNSIGNED DEFAULT '0')";
     $conn->exec($sql);
 }
 
@@ -31,28 +30,73 @@ function createTableStickers($conn) {
     $conn->exec($sql);
 }
 
+//returns value (ex. in users table $result[0] is userid)if it finds the qualifier, nothing otherwise
+function selectOneQualifier($conn, $table, $col, $qualifier) {
+    try {
+        $sql = $conn->prepare("SELECT * FROM `$table` WHERE $col = '$qualifier'");
+        $sql->execute();
+        $result = $sql->fetch();
+        return $result[0];
+    } catch(PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+        return false;
+    }
+}
+
+//checks if the username or email are already in use
+//returns 0 if not in use, 1 if username in use, 2 if email in use
+//helpful for debugging: echo $checkUserName;
+function isUserInTable($conn, $username, $email) {
+    $checkUserName = selectOneQualifier($conn, 'Users', 'userName', $username);
+    if ($checkUserName > 0)
+    {
+        echo "That username is already in table";
+        return 1;
+    }
+    $checkEmail = selectOneQualifier($conn, 'Users', 'email', $email);
+    if ($checkEmail > 0)
+    {
+         echo "That email is already in table";
+         return 2;
+    }
+    return 0;
+}
+
+//adds whatever info is passed, no checks here
 function addUserToTable($conn, $username, $email, $psswd) {
+    try {
+        $sql = $conn->prepare("INSERT INTO Users (userName, email, psswd)
+        VALUES ('$username', '$email', '$psswd')");
+        $sql->execute();
+        echo "User added to Users successfully";
+    } catch(PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+}
+
+//TODO check is photo in DB already
+function addImgToTable($conn, $userid, $imgname) {
     $sql = "";
     try {
-        $sql = "INSERT INTO Users (userName, email, psswd)
-        VALUES ('$username', '$email', '$psswd')";
+        $sql = "INSERT INTO Images (userId, imgName)
+        VALUES ('$userid', '$imgname')";
         $conn->exec($sql);
-//        echo "User added to Users successfully";
-      } catch(PDOException $e) {
+        //        echo "Image added to Users successfully";
+    } catch(PDOException $e) {
         echo $sql . "<br>" . $e->getMessage();
-      }
+    }
 }
 
 //TODO delete that users photos (comments&likes), then the user
-function deleteUserFromTable($conn, $username) {
-    $sql = "";
-    try {
-        $sql = "DELETE FROM Users
-        WHERE userId";
-        $conn->exec($sql);
-        echo "User deleted from Users successfully";
-      } catch(PDOException $e) {
-        echo $sql . "<br>" . $e->getMessage();
-      }
-}
+// function deleteUserFromTable($conn, $username) {
+    //     $sql = "";
+    //     try {
+        //         $sql = "SELECT * FROM (Images INNER JOIN Users ON Images.userId=Users.userId)
+        //         WHERE userName='$username'";
+        //         $conn->exec($sql);
+        //         echo "User deleted from Users successfully";
+        //       } catch(PDOException $e) {
+            //         echo $sql . "<br>" . $e->getMessage();
+            //       }
+            // }
 ?>
