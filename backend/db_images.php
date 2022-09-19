45db03fd8dede49e8797842e2abc930046fdfb57
Alba Castano <acastanome@gmail.com>
2022-09-19 22:17:10 +0300

    changed variable names

--- backend/db_images.php
+++ backend/db_images.php
@@ -1,11 +1,11 @@
 <?php
 
 //TODO check is photo in DB already. save photo to folder let path . name;
-function addImgToTable($conn, $userid, $imgname) {
+function addImgToTable($conn, $userId, $imgName, $imgPath) {
     try {
-        $sql = $conn->prepare("INSERT INTO Images (userId, imgName)
+        $sql = $conn->prepare("INSERT INTO Images (`user_id`, `img_name`, img_path)
         VALUES (?, ?)");
-        $sql->execute([$userid, $imgname]);
+        $sql->execute([$userId, $imgName, $imgPath]);
         echo "Image added to table successfully";
     } catch(PDOException $e) {
         echo "<br>" . $e->getMessage();
