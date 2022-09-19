68e6c74417ff30aca3784372878163ef1f7f2f70
Alba Castano <acastanome@gmail.com>
2022-09-19 22:21:13 +0300

    divided from index.php to frontend/header.php and frontend/footer.php

---
+++ frontend/footer.php
@@ -0,0 +1,5 @@
+</body>
+<footer>
+        <em>&copy; acastano | Hive 2022</em>
+    </footer>
+</html>
--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <?php
 require_once 'config/setup.php';
+include_once 'frontend/head.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Camagru</title>
-</head>
-<body>
-    <h1>WHAT</h1>
-    <?php
-    echo 'WHASSSUUUP';
-    ?>
-<h3>Welcome</h3>
-</body>
-<footer>
-<em>&copy; acastano | Hive 2022</em>
-</footer>
-</html>
+<h3>Welcome index</h3>
+
+<?php
+// header("Location: frontend/login.php");
+include 'frontend/footer.php';
+?>
