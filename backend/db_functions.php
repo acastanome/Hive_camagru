05899801e5e41f551706f542932bef0138d4acba
Alba Castano <acastanome@gmail.com>
2022-09-21 20:26:15 +0300

    removed header file and backend files, moved all functions that interact with db to backend/db_functions.php

--- backend/db_functions.php
+++ backend/db_functions.php
@@ -1,9 +1,41 @@
 <?php
 
+//returns value (ex. in users table $result[0] is userid)if it finds the qualifier, nothing otherwise
+function selectOneQualifier($conn, $table, $col, $qualifier) {
+    try {
+        $sql = $conn->prepare("SELECT * FROM $table WHERE $col = ?");
+        $sql->execute([$qualifier]);
+        $result = $sql->fetch();
+        return $result;//[0] would return userid but creates a warning with apache
+    } catch(PDOException $e) {
+        echo "<br>" . $e->getMessage();
+        return false;
+    }
+}
+
+
+//IMAGE FUNCTIONS
+
+//TODO check is photo in DB already. save photo to folder let path . name;
+function addImgToTable($conn, $userId, $imgName, $imgPath) {
+    try {
+        $sql = $conn->prepare("INSERT INTO Images (`user_id`, `img_name`, img_path)
+        VALUES (?, ?)");
+        $sql->execute([$userId, $imgName, $imgPath]);
+        echo "Image added to table successfully";
+    } catch(PDOException $e) {
+        echo "<br>" . $e->getMessage();
+    }
+}
+
+
+//USERS FUNCTIONS
+
 //checks if the username or email are already in use
 //returns 0 if not in use, 1 if username in use, 2 if email in use
 //helpful for debugging: echo $checkUserName;
-function isUserOrEmailInTable($conn, $username, $email) {
+function isUserOrEmailInTable($username, $email) {
+    $conn = connectPDODB();
     $checkUserName = selectOneQualifier($conn, 'Users', 'user_name', $username);
     if ($checkUserName > 0)
     {
@@ -20,7 +52,8 @@ function isUserOrEmailInTable($conn, $username, $email) {
 }
 
 //adds whatever info is passed, NO CHECKS here
-function addUserToTable($conn, $username, $email, $psswd) {
+function addUserToTable($username, $email, $psswd) {
+    $conn = connectPDODB();
     $code = $email.time();
     $activationCode = hash('whirlpool', $code);
     try {
@@ -34,7 +67,8 @@ function addUserToTable($conn, $username, $email, $psswd) {
 }
 
 //delete all images from a user. NO CHECKS, just looks for that users images and deletes them
-function deleteUserImagesFromTable($conn, $username) {
+function deleteUserImagesFromTable($username) {
+    $conn = connectPDODB();
     try {
         $sql = $conn->prepare("DELETE Images FROM (Images INNER JOIN Users ON Images.user_id=Users.user_id)
         WHERE `user_name`=?");
@@ -48,7 +82,8 @@ function deleteUserImagesFromTable($conn, $username) {
 
 //looks for the passed in usernames photos, deletes them, and then deletes the user
 //TODO delete that users comments&likes
-function deleteUserFromTable($conn, $username) {
+function deleteUserFromTable($username) {
+    $conn = connectPDODB();
     $checkUserName = selectOneQualifier($conn, 'Users', `user_name`, $username);
     if (!$checkUserName)
     {
@@ -77,7 +112,8 @@ function deleteUserFromTable($conn, $username) {
 }
 
 //NO CHECKS updates passed in usernames password to the one provided
-function changePasswd($conn, $username, $newPsswd) {
+function changePasswd($username, $newPsswd) {
+    $conn = connectPDODB();
     try {
         $sql = $conn->prepare("UPDATE `Users` SET `psswd` = ? WHERE `user_name` = ?");
         $sql->execute([$newPsswd, $username]);
@@ -88,7 +124,8 @@ function changePasswd($conn, $username, $newPsswd) {
 }
 
 //NO CHECKS updates passed in usernames email to the one provided
-function changeEmail($conn, $username, $newEmail) {
+function changeEmail($username, $newEmail) {
+    $conn = connectPDODB();
     try {
         $sql = $conn->prepare("UPDATE `Users` SET `email` = ? WHERE `user_name` = ?");
         $sql->execute([$newEmail, $username]);
@@ -99,7 +136,8 @@ function changeEmail($conn, $username, $newEmail) {
 }
 
 //NO CHECKS updates passed in username to the one provided
-function changeUsername($conn, $username, $newUsername) {
+function changeUsername($username, $newUsername) {
+    $conn = connectPDODB();
     try {
         $sql = $conn->prepare("UPDATE `Users` SET `user_name` = ? WHERE `user_name` = ?");
         $sql->execute([$newUsername, $username]);
@@ -109,7 +147,8 @@ function changeUsername($conn, $username, $newUsername) {
     }
 }
 
-function activateAccount($conn, $activationCode) {
+function activateAccount($activationCode) {
+    $conn = connectPDODB();
     try {
         $sql = $conn->prepare("UPDATE `Users` SET `confirmed_account` = '1' WHERE `activation_code` = ?");
         $sql->execute([$activationCode]);
@@ -121,11 +160,13 @@ function activateAccount($conn, $activationCode) {
 
 //checks if the username and password are matching
 //returns 0 if not valid, user_id if valid
-function isValidUser($conn, $username, $psswd) {
+function isValidUser($username, $psswd) {
+    $conn = connectPDODB();
     try {
         $sql = $conn->prepare("SELECT * FROM Users WHERE (`user_name` = ? AND `psswd` = ?)");
         $sql->execute([$username, $psswd]);
         $result = $sql->fetch();
+        $conn = null;
         if ($result)
             return $result[0];
     } catch(PDOException $e) {
