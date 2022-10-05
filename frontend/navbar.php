ae2398f57d1c78257c783661d7218ccb2ba6f402
Alba Castano <acastanome@gmail.com>
2022-10-06 02:03:05 +0300

    branch to work on css. simplified navbar html/css

--- frontend/navbar.php
+++ frontend/navbar.php
@@ -1,44 +1,34 @@
 <nav>
-      <div class="dropdown" id="out">
-        <button class="dropbtn">
-          <a href="home.php">Home</a>
-        </button>
+      <div class="dropdown" onclick="window.location='home.php';">
+        Home
       </div>
 
       <?php
-      if (!isset($_SESSION['logged_user']))
+      if (isset($_SESSION['logged_user']))
       {
       ?>
-      <!-- <h3>Not logged in. <br>Display navbar (Home, log in) and all photos(cant comment or like)</h3> -->
-      <div class="dropdown" id="out" style="float: right;">
-        <button class="dropbtn">
-          <a href="login.php">Log in</a>
-        </button>
+      <div class="dropdown" onclick="window.location='camera.php';">
+        Camera
       </div>
+
+      <div class="dropdown">
+        My profile
+        <div class="dropdown-content">
+          <a href="myphotos.php">My photos</a>
+          <a href="settings.php">Settings</a>
+        </div>
+      </div>
+
+      <button class="dropdown dropbtn" style="float: right;" onclick="window.location='logout.php';">
+        Log out
+      </button>
       <?php
       }
       else
       {
       ?>
-      <!-- <h3>Display navbar (Home, camera, profile, log out) and all photos(can comment or like)</h3> -->
-      <div class="dropdown" id="out">
-        <button class="dropbtn">
-        <a href="camera.php">Camera</a>
-        </button>
-      </div>
-      <div class="dropdown" id="out">
-        <button class="dropbtn">
-          <a href="profile.php">My profile</a>
-        </button>
-        <div class="dropdown-content" id="out1">
-          <a>My photos</a>
-          <a>Settings</a>
-        </div>
-      </div>
-      <div class="dropdown" id="out" style="float: right;">
-        <button class="dropbtn">
-          <a href="logout.php">Log out</a>
-        </button>
+      <div class="dropdown" style="float: right;" onclick="window.location='login.php';">
+        Log in
       </div>
       <?php
       }
