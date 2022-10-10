14955f48b7b958cca35cb5d77a2c4154edbbd71a
Alba Castano <acastano@student.hive.fi>
2022-10-10 20:43:34 +0300

    Cleaned extra comments from frontend/active.php

--- frontend/activate.php
+++ frontend/activate.php
@@ -31,17 +31,4 @@
  ?>
 </body>
 
-<?php
-	// require_once("dbcontroller.php");
-	// $db_handle = new DBController();
-	// if(!empty($_GET["id"])) {
-	// $query = "UPDATE registered_users set status = 'active' WHERE id='" . $_GET["id"]. "'";
-	// $result = $db_handle->updateQuery($query);
-	// 	if(!empty($result)) {
-	// 		$message = "Your account is activated.";
-	// 	} else {
-	// 		$message = "Problem in account activation.";
-	// 	}
-	// }
-?>
 <?php require_once 'footer.php';?>
