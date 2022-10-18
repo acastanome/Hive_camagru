f38fa23e794cb4fca5a20a1f82d22d793a98d4d1
Alba Castano <acastano@student.hive.fi>
2022-10-18 17:22:50 +0300

    changed link being sent (camgru, not camagru_git)

--- backend/db_email.php
+++ backend/db_email.php
@@ -16,22 +16,17 @@ function deleteUserSimple($email) {
 }
 
 function sendActivationEmail($email, $activationCode) {
-    // $actual_link = "http://localhost:8080/frontend/activate.php?user_id=" . $user_id;
-    $actual_link = "http://localhost:8080/camagru_git/frontend/activate.php?activation_code=" . $activationCode;
+    $actual_link = "http://localhost:8080/camagru/frontend/activate.php?activation_code=" . $activationCode;
     $toEmail = $email;
     $subject = "User Registration Activation Email";
     $content = "Click this link: " . $actual_link . " to activate your account.";
-    // $mailHeaders = "From: Camagru Admin\r\n";
-    // if(mail($toEmail, $subject, $content, $mailHeaders)) {
     if(mail($toEmail, $subject, $content)) {
-        // $message = "You have registered and the activation mail is sent to your email. Click the activationlink to activate you account.";
         return nl2br("Registration has been successfull!\nTo activate your account click the link sent to: " . $toEmail);
     }
     else {
         deleteUserSimple($email);
         return "Problem in registration. Please try again!";
     }
-    // unset($_POST);
 }
 
 ?>
