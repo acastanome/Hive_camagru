cdae2388f5877099e35cbe70f4b9ed5ba681892e
Alba Castano <acastano@student.hive.fi>
2022-10-22 22:19:42 +0300

    added functions to getNBPosts() and fetch_images() to display gallery

--- backend/db_gallery.php
+++ backend/db_gallery.php
@@ -2,19 +2,19 @@
 
 require_once 'db_connect.php';
 
-function getAllImages() {
-    $conn = connectPDODB();
-    try {
-        $sql = $conn->prepare("SELECT * FROM `Images`");
-        $sql->execute();
-        $result = $sql->fetch();
-        $conn = null;
-        return $result;//[0] would return userid but creates a warning with apache
-    } catch(PDOException $e) {
-        echo "<br>" . $e->getMessage();
-        return false;
-    }
-}
+// function getAllImages() {
+//     $conn = connectPDODB();
+//     try {
+//         $sql = $conn->prepare("SELECT * FROM `Images`");
+//         $sql->execute();
+//         $result = $sql->fetch();
+//         $conn = null;
+//         return $result;//[0] would return userid but creates a warning with apache
+//     } catch(PDOException $e) {
+//         echo "<br>" . $e->getMessage();
+//         return false;
+//     }
+// }
 
 //TODO check is photo in DB already. save photo to folder let path . name;
 function addImgToTable($userId, $imgName, $imgPath) {
@@ -34,4 +34,29 @@ function addImgToTable($userId, $imgName, $imgPath) {
     }
 }
 
+function getNbPosts() {
+    $conn = connectPDODB();
+    try {
+        $sql = $conn->prepare("SELECT COUNT(*) FROM `Images`");
+        $sql->execute();
+        $result = $sql->fetch();
+        $conn = null;
+        if ($result)
+            return $result[0];
+    } catch(PDOException $e) {
+        echo "<br>" . $e->getMessage();
+    }
+}
+
+function fetch_images(int $first, int $amount) {
+    $conn = connectPDODB();
+    try {
+      $sql = $conn->prepare("SELECT * FROM `Images` ORDER BY `img_id` DESC LIMIT $first, $amount");
+      $sql->execute();
+      $result = $sql->fetchAll();
+      return $result;
+    } catch (PDOException $e) {
+      echo "Error: " . $e->getMessage();
+    }
+  }
 ?>
