1d7e8b0f83931be0362af91d73c0347840f03d19
Alba Castano <acastano@student.hive.fi>
2022-10-26 01:17:42 +0300

    in case of emergency files

---
+++ lastworking/uploadsworking/upload.php
@@ -0,0 +1,16 @@
+
+	<link rel="stylesheet" href="css/cam.css">
+
+
+			<div class="camera_container">
+				<div>
+					<input type="file" id="imageInput" accept = "image/*" class="dropbtn">
+					<br><br>
+					
+    				<canvas class="canvas" id= "myCanvas" width="320" height="240"></canvas>
+					<canvas class="canvas" id="canvas_stickers" width="320" height="240"></canvas>
+    			</div>
+			</div>
+
+
+	<script type="text/javascript" src="js/js_upload.js" charset="utf-8"></script>
---
+++ lastworking/uploadsworking/upload_create_post.php
@@ -0,0 +1,101 @@
+<?php require_once 'head.php'; ?>
+
+<?php
+if (isset($_SESSION['logged_user']) && isset($_SESSION['logged_id'])) {
+
+    require_once '../backend/db_user.php';
+    require_once '../backend/db_gallery.php';
+    // if (isset($_POST['post_photo']) && !empty($_POST['webcam_file']) && !empty($_POST['stickers_file'])) {
+    //     echo "in camera upl";
+    //     $webcam_file = $_POST['webcam_file'];
+    //     $stickers_file = $_POST['stickers_file'];
+    //     $image_name = strval(time()) . ".jpg";
+
+	//     list($type, $url_webcam) = explode(';', $webcam_file);
+	//     list(, $url_webcam) = explode(',', $url_webcam); 
+	//     $cam = imagecreatefromstring(base64_decode($url_webcam));
+
+	//     list($type, $url_stickers) = explode(';', $stickers_file);
+	//     list(, $url_stickers) = explode(',', $url_stickers); 
+	//     $stickers = imagecreatefromstring(base64_decode($url_stickers));
+
+    //     if ($cam && $stickers) {
+    //         imagecopy($cam, $stickers, 0, 0, 0, 0, 700, 500);
+    //         imagepng($cam, "../images/".$image_name);
+    //         $path = "http://localhost:8080/camagru/images/". $image_name;
+    //         addImgToTable($_SESSION['logged_id'], $image_name, $path);
+    //     }
+    //     else {
+    //         ?>
+             <!-- <script type="text/javascript"> -->
+                 <!-- console.log("Base64 value isn't a valid image."); -->
+             <!-- </script> -->
+             <?php
+    //     }
+    // } 
+
+    if (isset($_POST['post_photo']) && isset($_FILES['file']['name']) && !empty($_FILES['file']['name'])) {
+        // upload_form['file'].files[0];
+        echo "whaaaaat" . $_FILES['file']['name'] . "full path:" . $_FILES['file']['full_path'] . "tmp_name:" . $_FILES['file']['tmp_name'];
+        // print_r(array_keys( $_FILES['file']));
+        //name [1] => full_path [2] => type [3] => tmp_name [4] => error [5] => size
+    }
+
+    // else if (isset($_POST['post_photo']) && !empty($_POST['myImg_file'])) {//upload
+    //     echo "in file upload";
+    //     $myImg_file = $_POST['myImg_file'];
+    //     $image_name = strval(time()) . ".jpg";
+        
+	//     list($type, $url_myIng) = explode(';', $myImg_file);
+	//     list(, $url_myIng) = explode(',', $url_myIng); 
+	//     $upload = imagecreatefromstring(base64_decode($url_myIng));
+        
+    //     if (!empty($_POST['stickers_file'])) {
+    //         $stickers_file = $_POST['stickers_file'];
+    //         list($type, $url_stickers) = explode(';', $stickers_file);
+    //         list(, $url_stickers) = explode(',', $url_stickers); 
+    //         $stickers = imagecreatefromstring(base64_decode($url_stickers));
+    //     }
+        
+    //     if ($upload && $stickers) {
+    //         imagecopy($upload, $stickers, 0, 0, 0, 0, 700, 500);
+    //         imagepng($upload, "../images/".$image_name);
+    //         $path = "http://localhost:8080/camagru/images/". $image_name;
+    //         addImgToTable($_SESSION['logged_id'], $image_name, $path);
+    //     }
+    //     else if ($upload) {
+    //         imagepng($upload, "../images/".$image_name);
+    //         $path = "http://localhost:8080/camagru/images/". $image_name;
+    //         addImgToTable($_SESSION['logged_id'], $image_name, $path);
+    //     }
+    //     else {
+    //         ?>
+             <!-- <script type="text/javascript"> -->
+                 <!-- console.log("Base64 value isn't a valid image."); -->
+    <!-- //         </script> -->
+             <?php
+    //     }
+    // // header("Location: camera.php");
+    // } else {
+    //     echo "in end else";
+    //     // header("Location: login.php");
+    //     // header("Location: test.php");
+    //     if (isset($_POST['post_photo'])) {
+    //         echo "post_photo:    ";
+    //         echo $_POST['post_photo'];
+    //     }
+    //     if(!empty($_POST['webcam_file'])) {
+    //         echo "  webcam_file:   ";
+    //         echo $_POST['webcam_file'];
+    //     }
+    //     if (!empty($_POST['stickers_file'])) {
+    //         echo "  stickers_file: ";
+    //         echo $_POST['stickers_file'];
+    //     }
+    //     if (!empty($_POST['myImg'])) {
+    //         echo "  myImg: ";
+    //         echo $_POST['myImg'];
+    //     }
+    // }
+}
+?>
