9eb0e4d6b1b4e011a6aafb207e6f3aa2d8414bd3
Alba Castano <acastano@student.hive.fi>
2022-10-27 05:38:44 +0300

    added My profile to navbar and fixed url

--- frontend/navbar.php
+++ frontend/navbar.php
@@ -11,17 +11,12 @@
     Camera
   </div>
 
-  <div class="dropdown">
-    My profile
-    <div class="dropdown-content">
-      <a href="myphotos.php">My photos</a>
-      <a href="settings.php">Settings</a>
-    </div>
-  </div>
-
   <button class="dropdown dropbtn" style="float: right;" onclick="window.location='logout.php';">
     Log out
   </button>
+  
+  <div class="dropdown" style="float: right;" onclick="window.location='profile.php';">My profile</div>
+
   <?php
   }
   else
