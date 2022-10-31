9817ee3c218969e3438beae7e945cbcf358cd507
Alba Castano <acastanome@gmail.com>
2022-10-31 14:58:06 +0200

    fixing not commenting on 2 posts if messing with js

--- frontend/comments.php
+++ frontend/comments.php
@@ -7,15 +7,12 @@ if (isset($_POST['comment'])){
 	$img_id = $_POST['image_comment'];
     $comment = $_POST['comment_status'];
     $commentText = $_POST['comment_text'];
-	if ($comment = 'like'){
+	if ($comment == 'like' && (strlen($commentText) > 0) && (strlen($commentText) <= 2000)) {
         if (commentImg($_SESSION['logged_id'], $img_id, $commentText)) {
             $usercommented = 1;
             $userToNotify = getUserIdFromImageId($img_id);
             if (getNotifications($userToNotify) == 1) {
-                echo "notifications on";
                 sendNotificationEmail(getEmailFromId($userToNotify));
-            } else {
-                echo "notifications off";
             }
         }
 	}
