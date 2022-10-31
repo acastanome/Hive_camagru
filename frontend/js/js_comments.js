80462dc2b194154dc706d387f0b1b073d74a4f9f
Alba Castano <acastanome@gmail.com>
2022-10-31 15:03:52 +0200

    encodeURIComponent so comments with & get saved and displayed correctly

--- frontend/js/js_comments.js
+++ frontend/js/js_comments.js
@@ -9,7 +9,7 @@ function ajaxComment(img_id) {
 			"comment=1&image_comment=" +
 			img_id +
 			"&comment_status=like&comment_text=" +
-			text;
+			encodeURIComponent(text);
 	} else if (status == "outlineC") {
 		imageComment.src =
 			"http://localhost:8080/camagru/stickers/comment-full.png";
@@ -18,7 +18,7 @@ function ajaxComment(img_id) {
 			"comment=1&image_comment=" +
 			img_id +
 			"&comment_status=like&comment_text=" +
-			text;
+			encodeURIComponent(text);
 	} else {
 		return;
 	}
@@ -26,25 +26,3 @@ function ajaxComment(img_id) {
 	xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xml.send(data);
 }
-// function ajaxComment(img_id) {
-// 	let xml = new XMLHttpRequest();
-// 	let imageComment = document.getElementById("comment" + img_id);
-// 	let text = document.getElementById("f_comment" + img_id).value;
-// 	let status = imageComment.name;
-
-// 	if (status == "full") {
-// 		imageComment.src =
-// 			"http://localhost:8080/camagru/stickers/comment-full.png";
-// 		imageComment.name = "outline";
-// 		data = "comment=1&image_comment=" + img_id + "&comment_status=like";
-// 	} else if (status == "outline") {
-// 		imageComment.src =
-// 			"http://localhost:8080/camagru/stickers/comment-outline.png";
-// 		imageComment.name = "full";
-// 		data = "comment=1&image_comment=" + img_id + "&comment_status=dislike";
-// 	}
-
-// 	xml.open("post", "comments.php?" + data, true);
-// 	xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-// 	xml.send(data);
-// }
