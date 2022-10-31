53c9312f5f6084a9485e4b9dafa658cbfe4a40f8
Alba Castano <acastano@student.hive.fi>
2022-10-31 18:25:00 +0200

    added protection to unlogged user

--- frontend/like.php
+++ frontend/like.php
@@ -2,16 +2,20 @@
 require_once "head.php";
 require_once "../backend/db_likes.php";
 
-if (isset($_POST['like'])){
-	$img_id = $_POST['image_heart'];
-    $heart = $_POST['heart_status'];
-	if ($heart == 'like' && checkUserLikedImg($_SESSION['logged_id'], $img_id)==0){
-    likeImg($_SESSION['logged_id'], $img_id);
-    $userliked = 1;
-	} 
-    else if ($heart == 'dislike' && checkUserLikedImg($_SESSION['logged_id'], $img_id)!=0){
-    unlikeImg($_SESSION['logged_id'], $img_id);
-    $userliked = 0;
-	}
+if (isset($_SESSION['logged_user'])) {
+    if (isset($_POST['like'])){
+    	$img_id = $_POST['image_heart'];
+        $heart = $_POST['heart_status'];
+    	if ($heart == 'like' && checkUserLikedImg($_SESSION['logged_id'], $img_id)==0){
+        likeImg($_SESSION['logged_id'], $img_id);
+        $userliked = 1;
+    	} 
+        else if ($heart == 'dislike' && checkUserLikedImg($_SESSION['logged_id'], $img_id)!=0){
+        unlikeImg($_SESSION['logged_id'], $img_id);
+        $userliked = 0;
+    	}
+    }
+} else {
+    header("Location: login.php");
 }
 ?>
