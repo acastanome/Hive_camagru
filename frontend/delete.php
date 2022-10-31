6c63fc428137fcd9da221eb0a7fbafb03054f30c
Alba Castano <acastano@student.hive.fi>
2022-10-31 18:24:28 +0200

    changed post deletion to use a form

--- frontend/delete.php
+++ frontend/delete.php
@@ -2,21 +2,14 @@
 require_once "head.php";
 require_once "../backend/db_gallery.php";
 
-?><script>console.log("out");</script><?php
-if (isset($_POST['like'])){
-	$img_id = $_POST['image_id'];
-    ?><script>console.log("in");</script><?php
-// }
-
-// if (isset($_POST['delete'])){
-// 	$imgId = $_POST['image_id'];
-// 	if (userOwnsImage($_SESSION['logged_id'], $imgId)) {
-//         deletePost($imgId);
-// 	} else { ?>
-//         <!-- <script>alert("Not your image, you trickster!");</script> -->
-//         <?php
-//     }
+if (isset($_SESSION['logged_user'])) {
+    if (isset($_POST['delete_image'])){
+        $imgId = $_POST['delete_image_id'];
+    	if (userOwnsImage($_SESSION['logged_id'], $imgId)) {
+            deletePost($imgId);
+        }
+    }
+    header("Location: add.php");
 } else {
-    ?><script>console.log("else");</script><?php
+    header("Location: login.php");
 }
-?>
--- frontend/js/js_delete.js
+++ frontend/js/js_delete.js
@@ -1,12 +1,6 @@
-function postDelete(img_id) {
-	let xml = new XMLHttpRequest();
+function confirmDelete() {
+	let agree = confirm("Are you sure you wish to delete this picture?");
 
-	// data = "delete=1&imgId=" + img_id;
-	// data = "delete=1&image_id=" + img_id + "";
-	data = "like=1&image_id=" + img_id + "&heart_status=like";
-	xml.open("post", "delete.php?" + data, true);
-	xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	console.log(data);
-	// console.log(data);
-	xml.send(data);
+	if (agree) return true;
+	else return false;
 }
