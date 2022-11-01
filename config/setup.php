c835e96d12a1896e078c58876c6e071ca6adce12
Alba Castano <acastano@student.hive.fi>
2022-11-01 15:52:55 +0200

    cleanup of testing functions

--- config/setup.php
+++ config/setup.php
@@ -31,66 +31,6 @@ try {
 }
 createTables($conn);
 
-//TEST PURPOSES ONLY FROM HERE
-$username = "testuser1";
-$email = "camagrutesting@proton.me";
-$psswd = "testpass1!";
-
-function selectOneQualifier($conn, $table, $col, $qualifier) {
-    try {
-        $sql = $conn->prepare("SELECT * FROM $table WHERE $col = ?");
-        $sql->execute([$qualifier]);
-        $result = $sql->fetch();
-        return $result;//[0] would return userid but creates a warning with apache
-    } catch(PDOException $e) {
-        echo "<br>" . $e->getMessage();
-        return false;
-    }
-}
-
-function isUserOrEmailTaken($conn, $username, $email) {
-    // $conn = connectPDODB();
-    try {
-        $sql = $conn->prepare("SELECT * FROM Users WHERE (`user_name` = ?)");
-        $sql->execute([$username]);
-        $result = $sql->fetch();
-        if ($result) {
-            return $result[0];
-        }
-        $sql = $conn->prepare("SELECT * FROM Users WHERE (`email` = ?)");
-        $sql->execute([$email]);
-        $result = $sql->fetch();
-        $conn = null;
-        if ($result) {
-            return $result[0];
-        }
-    } catch(PDOException $e) {
-        echo "<br>" . $e->getMessage();
-    }
-}
-
-//adds whatever info is passed, NO CHECKS here
-function addUserToTable($conn, $username, $email, $psswd) {
-    // $conn = connectPDODB();
-    $psswd = password_hash($psswd, PASSWORD_BCRYPT);
-    $code = $email.time();
-    $activationCode = password_hash($code, PASSWORD_BCRYPT);
-    try {
-        $sql = $conn->prepare("INSERT INTO Users (`user_name`, `email`, psswd, activation_code)
-        VALUES (?, ?, ?, ?)");
-        $sql->execute([$username, $email, $psswd, $activationCode]);
-        $conn = null;
-        // echo "User added to Users successfully";
-    } catch(PDOException $e) {
-        echo "<br>" . $e->getMessage();
-    }
-}
-
-if (isUserOrEmailTaken($conn, $username, $email) == 0){
-    addUserToTable($conn, $username, $email, $psswd);
-}
-//TO HERE
-
 $conn = null;
 
 ?>
