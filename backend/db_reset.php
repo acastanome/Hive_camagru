4621406a62eec55db5859a17a0227601ac11b9ee
Alba Castano <acastano@student.hive.fi>
2022-11-01 19:58:02 +0200

    erased commented out code

--- backend/db_reset.php
+++ backend/db_reset.php
@@ -2,20 +2,17 @@
 require_once 'db_connect.php';
 
 function getUserIdFromUsernameOrEmail($input) {
-    // if (preg_match('/^(?=.*[@])$/',$input)) {
-        $conn = connectPDODB();
-        try {
-            $sql = $conn->prepare("SELECT `user_id` FROM Users WHERE (`user_name` = ? OR `email` = ?)");
-            $sql->execute([$input, $input]);
-            $result = $sql->fetch();
-            if ($result) {
-                return $result[0];
-            }
-        } catch(PDOException $e) {
-            echo "<br>" . $e->getMessage();
+    $conn = connectPDODB();
+    try {
+        $sql = $conn->prepare("SELECT `user_id` FROM Users WHERE (`user_name` = ? OR `email` = ?)");
+        $sql->execute([$input, $input]);
+        $result = $sql->fetch();
+        if ($result) {
+            return $result[0];
         }
-    // }
-    // return true;
+    } catch(PDOException $e) {
+        echo "<br>" . $e->getMessage();
+    }
 }
 
 function getEmailFromUserId($uid) {
@@ -36,13 +33,11 @@ function getEmailFromUserId($uid) {
 function getActivationCodeFromUserId($uid) {
     $conn = connectPDODB();
     try {
-        // echo "PROBLEM 1";
         $sql = $conn->prepare("SELECT `activation_code` FROM Users WHERE (`user_id` = ?)");
         $sql->execute([$uid]);
         $result = $sql->fetch();
         $conn = null;
         if ($result) {
-            // echo "PROBLEM 2 $result";
             return $result[0];
         }
     } catch(PDOException $e) {
@@ -51,9 +46,7 @@ function getActivationCodeFromUserId($uid) {
 }
 
 function sendResetEmail($uid) {
-    // getEmailFromUserId($uid);
     $activationCode = getActivationCodeFromUserId($uid);
-    // echo "PROBLEM $activationCode";
 
     $actual_link = "http://localhost:8080/camagru/frontend/reset_pass.php?reset_code=" . $activationCode;
     $toEmail = getEmailFromUserId($uid);
@@ -65,6 +58,5 @@ function sendResetEmail($uid) {
     else {
         return "Problem in password reset. Please try again!";
     }
-    // unset($_POST);
 }
 ?>
