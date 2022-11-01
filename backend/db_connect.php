3dd3d3f26adc82392315fe40447bf254523265df
Alba Castano <acastano@student.hive.fi>
2022-11-01 19:56:54 +0200

    erased commented out code

--- backend/db_connect.php
+++ backend/db_connect.php
@@ -1,4 +1,4 @@
-<?php //check with backend
+<?php
 
 function connectPDODB(){
     try {
@@ -10,14 +10,4 @@ function connectPDODB(){
     return $conn;
 }
 
-    // $DB_NAME = "db_camagru";
-    // $DB_DSN = "mysql:host=localhost";
-    // $DB_USER = "root";//"username";
-    // $DB_PASSWORD = "123456";//"password";
-    // try {
-    //     $conn = new PDO("$DB_DSN;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
-    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // } catch(PDOException $e) {
-    //     echo "Connection failed: " . $e->getMessage();
-    // }
 ?>
