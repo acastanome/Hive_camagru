b414ed81013f8d1f26513926c61547ed77749b16
Alba Castano <acastano@student.hive.fi>
2022-11-01 15:53:16 +0200

    comments deleted

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 $DB_NAME = "db_camagru";
 $DB_DSN = "mysql:host=localhost";
-$DB_USER = "root";//"username";
-$DB_PASSWORD = "123456";//"password";
+$DB_USER = "root";
+$DB_PASSWORD = "123456";
 
 ?>
